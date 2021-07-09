20a1775a4a60f99fc2ec983f78ff41d8285e1f56
CT702-7 <836915468@qq.com>
2021-07-10 01:40:54 +0800

    7.10 morning update(-q)

--- ping.c
+++ ping.c
@@ -1,12 +1,31 @@
 #include "ping.h"            
 
+/*
+ * 为IPv4和IPv6分别定义一个proto结构
+ * 套接字地址结构指针置空，因为不知道最终使用IPv4还是IPv6
+ */
 struct proto	proto_v4 = { proc_v4, send_v4, NULL, NULL, 0, IPPROTO_ICMP };
 
 #ifdef	IPV6
 struct proto	proto_v6 = { proc_v6, send_v6, NULL, NULL, 0, IPPROTO_ICMPV6 };
 #endif
 
+/* 回射请求发送的可选数据量长度 */
 int	datalen = 56;		/* data that goes with ICMP echo request */
+
+// 指示安静模式停止的指示位
+int quitFlag = 0;
+// -q模式下接收到ctrl+c指令后输出结果的函数
+void quiteShowResult(int sig) {
+	// 判断当前是否为-q指令
+		if(quite) {
+			// 计算丢包率
+			quitFlag = 1;
+			double loss = (double)(quitePackageTotal - quitePackageTotal) / quitePackageTotal;
+			printf("\n--- %s ping statistics ---\n", quiteTargetName);
+			printf("%d packats transmitted, %d received, %.0lf%% packet loss", quitePackageTotal, quitePackageSuccess, loss);	
+	}
+}
 
 int
 main(int argc, char **argv)
@@ -14,11 +33,19 @@ main(int argc, char **argv)
 	int	c;
 	struct addrinfo	*ai;
 
+	/*
+	 * 处理命令行参数
+	 * -v	显示接收到的ICMP消息
+	 * 		
+	 */
 	opterr = 0;		/* don't want getopt() writing to stderr */
-	while ( (c = getopt(argc, argv, "v")) != -1) {
+	while ( (c = getopt(argc, argv, "vq")) != -1) {
 		switch (c) {
 		case 'v':
 			verbose++;
+			break;
+		case 'q':
+			quite++;
 			break;
 		case '?':
 			err_quit("unrecognized option: %c", c);
@@ -32,12 +59,21 @@ main(int argc, char **argv)
 	pid = getpid();
 	signal(SIGALRM, sig_alrm);
 
+	/*
+	 * 处理主机名参数
+	 * 命令行中必须有一个主机名获IP地址参数，由host_serv处理
+	 * 
+	 */
 	ai = host_serv(host, NULL, 0, 0);
 
+	// 向安静模式传输IP或者主机名
+	quiteTargetName = ai->ai_canonname;
 	printf("ping %s (%s): %d data bytes\n", ai->ai_canonname,
 		Sock_ntop_host(ai->ai_addr, ai->ai_addrlen), datalen);
 
-		/* 4initialize according to protocol */
+	/* 
+	 * 让全局指针变量pr指向正确的proto结构 
+	 */
 	if (ai->ai_family == AF_INET) {
 		pr = &proto_v4;
 #ifdef	IPV6
@@ -54,6 +90,7 @@ main(int argc, char **argv)
 	pr->sarecv = calloc(1, ai->ai_addrlen);
 	pr->salen = ai->ai_addrlen;
 
+	/* 无限循环 */
 	readloop();
 
 	exit(0);
@@ -68,6 +105,10 @@ proc_v4(char *ptr, ssize_t len, struct timeval *tvrecv)
 	struct icmp		*icmp;
 	struct timeval	*tvsend;
 
+	/* 
+	 * 得到IPv4首部为多少字节
+	 * 将指针指向ICMP首部开始的位置
+	 */
 	ip = (struct ip *) ptr;		/* start of IP header */
 	hlen1 = ip->ip_hl << 2;		/* length of IP header */
 
@@ -75,24 +116,43 @@ proc_v4(char *ptr, ssize_t len, struct timeval *tvrecv)
 	if ( (icmplen = len - hlen1) < 8)
 		err_quit("icmplen (%d) < 8", icmplen);
 
-	if (icmp->icmp_type == ICMP_ECHOREPLY) {
+	/* 
+	 * 检查标识符字段
+	 * 判断该应答是否是本进程发出的请求
+	 */
+	if (icmp->icmp_type == ICMP_ECHOREPLY && !quite) {
 		if (icmp->icmp_id != pid)
 			return;			/* not a response to our ECHO_REQUEST */
 		if (icmplen < 16)
 			err_quit("icmplen (%d) < 16", icmplen);
-
+	
+		/* 计算rtt（往复时间）*/
 		tvsend = (struct timeval *) icmp->icmp_data;
 		tv_sub(tvrecv, tvsend);
 		rtt = tvrecv->tv_sec * 1000.0 + tvrecv->tv_usec / 1000.0;
 
+		/* 打印信息 */
 		printf("%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f ms\n",
 				icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
 				icmp->icmp_seq, ip->ip_ttl, rtt);
 
-	} else if (verbose) {
+	} 
+	/* 设置了-v（详尽输出）*/
+	else if (verbose) {
 		printf("  %d bytes from %s: type = %d, code = %d\n",
 				icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
 				icmp->icmp_type, icmp->icmp_code);
+	}
+
+	/* 设置了-q（安静输出）*/
+	else if (quite) {
+		// icmp->icmp_seq + 1 即传输包总数
+		quitePackageTotal = icmp->icmp_seq + 1; 
+
+		// icmp->icmp_type = 3 的个数即传输成功的包的个数
+		if(icmp->icmp_type == 8)
+			quitePackageSuccess++;
+		// printf("seq:%d\n", icmp->icmp_seq);
 	}
 }
 
@@ -218,15 +278,34 @@ readloop(void)
 	ssize_t			n;
 	struct timeval	tval;
 
+	/* 船舰一个合适协议的原始套接字 */
 	sockfd = socket(pr->sasend->sa_family, SOCK_RAW, pr->icmpproto);
 	setuid(getuid());		/* don't need special permissions any more */
 
+	/* 
+	 * 将套接字接收缓冲区设置得比默认值大
+	 * 防止用户对IPv4广播地址执行ping
+	 * 造成大量应答使接收缓冲区溢出
+	 */
 	size = 60 * 1024;		/* OK if setsockopt fails */
 	setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
 
+	/* 
+	 * 发送第一个分组
+	 * 调度下一个SIGALRM信号在1秒后产生
+	 */
 	sig_alrm(SIGALRM);		/* send first packet */
 
-	for ( ; ; ) {
+	/* 
+	 * 无限循环
+	 * 读入返回的每个分组
+	 */
+	/* 
+	 * 循环结束
+	 * 若处于-q则应该输出结果
+	 */
+	signal(SIGINT, quiteShowResult);
+	for ( ; !quitFlag; ) {
 		len = pr->salen;
 		n = recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, pr->sarecv, &len);
 		if (n < 0) {
@@ -236,6 +315,10 @@ readloop(void)
 				err_sys("recvfrom error");
 		}
 
+		/* 
+		 * 记录分组收取时刻
+		 * 调用合适的协议函数（proc_v4或者proc_v6）处理包含在该分组中的ICMP消息
+		 */
 		gettimeofday(&tval, NULL);
 		(*pr->fproc)(recvbuf, n, &tval);
 	}
